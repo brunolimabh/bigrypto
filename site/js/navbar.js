cc7f179a1c55380b0a0a62b3da7043e98b760f71
brunolimabh <bruno.lima@sptech.school>
2023-05-10 21:20:41 -0300

    validação

--- site/js/navbar.js
+++ site/js/navbar.js
@@ -5,16 +5,19 @@ const listaNav = document.getElementById('listaNav')
 window.addEventListener('scroll', ()=>{
     if(scrollY <= 100){
         navbar.style = 'background-color = none'
+        navbar.style.boxShadow = "0px 0px 0px black";
         listaNav.classList.add('nav-lista-vazio');
         listaNav.classList.remove('nav-lista-laranja');
-        listaNav.classList.remobe('nav-lista-preto');
+        listaNav.classList.remove('nav-lista-preto');
     } else if (scrollY >= 1400 && scrollY <= 2725 || scrollY >=  3326 && scrollY <= 4000 ) {
-        navbar.style.backgroundColor = 'black'
+        navbar.style.backgroundColor = 'black';
+        navbar.style.boxShadow = "0px 3px 6px black";
         listaNav.classList.remove('nav-lista-vazio');
         listaNav.classList.remove('nav-lista-laranja');
         listaNav.classList.add('nav-lista-preto');
     } else {
         navbar.style.backgroundColor = 'rgb(255, 180, 42)'
+        navbar.style.boxShadow = "0px 3px 6px black";
         listaNav.classList.remove('nav-lista-vazio');
         listaNav.classList.add('nav-lista-laranja');
         listaNav.classList.remove('nav-lista-preto');
--- site/js/simulacao.js
+++ site/js/simulacao.js
@@ -75,9 +75,11 @@ function simular() {
 
     total_atual = totalMoeda * valor_cripto_atual;
 
-    if (reais == '') {
-        
+    if (reais == '' || isNaN(reais)) {
+        erroReal.style.display = 'block';
     } else {
+        erroReal.style.display = 'none';
+
         if (cripto == "Solana" && data.value == "2017" || cripto == "Solana" && data.value == "2018" || cripto == "Solana" && data.value == "2019") {
             msg_simulacao.innerHTML = `Caso deseje simular uma cotação da moeda Solana, opte pelos anos acima de 2020, pois foi o seu ano de lançamento!`;
             ano_simulacao.innerHTML = ``;
